160fd37e405ba1e9978bc162bcb71d5628eef412
Hema123kamadolli <hema.kamadolli@mphasis.com>
2024-01-01 15:14:43 +0530

    Initial Commit

---
+++ Assignment_12/Data/Assignment_12DbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_12.Data
+{
+    public class Assignment_12DbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Assignment_12DbContext() : base("name=Assignment_12DbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Assignment_12.Models.Cricket> Crickets { get; set; }
+    }
+}
---
+++ Assignment_12/Models/Cricket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_12.Models
+{
+    
+    public class Cricket
+    {
+        [Key]
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NOWC { get; set; }
+    }
+}
